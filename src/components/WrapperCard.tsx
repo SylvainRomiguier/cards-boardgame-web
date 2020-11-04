4c0e96cf6c93ec31d9f0d254907fae0de2fa28ba
Sylvain Romiguier <sylvain.romiguier@gmail.com>
2020-11-04 08:39:10 +0100

    WrapperCard creation and styling

---
+++ src/components/WrapperCard.tsx
@@ -0,0 +1,29 @@
+import { Box } from "@chakra-ui/core";
+import React from "react";
+
+interface WrapperCardProps {
+  background?: string;
+}
+
+export const WrapperCard: React.FC<WrapperCardProps> = ({
+  children,
+  background,
+}) => {
+  return (
+    <Box w="340px" h="474px">
+      <div
+        style={{
+          backgroundImage: `url(${background})`,
+          width: "100%",
+          height: "100%",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "contain",
+          padding: "40px",
+          paddingTop: "60px"
+        }}
+      >
+        {children}
+      </div>
+    </Box>
+  );
+};
